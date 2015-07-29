1f8da37fea826219d8a37d1ca24d09833c861e65
Jose Chirivella <jrchg1403@gmail.com>
2015-07-28 23:06:06 -0400

    added routes. Added index on home views and also added on the index

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  get 'projects/index'
+  resources :home
 
-  get 'projects/show'
+  resources :projects
 
-  get 'projects/new'
-
-  get 'projects/create'
-
-  get 'projects/update'
-
-  get 'projects/destroy'
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
