417525edf3fa6b2ffbb1403dc8b8f0d76d940ef5
Jose Chirivella <jrchg1403@gmail.com>
2015-09-05 17:03:38 -0400

    Added validations in contacts form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :text, presence: true
 end
