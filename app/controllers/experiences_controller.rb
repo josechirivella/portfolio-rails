336a33f3c12bc2c76ea54d274677aece603f4e50
Jose Chirivella <jrchg1403@gmail.com>
2015-10-06 00:07:01 -0400

    Added params and configs to education and experience controller

---
+++ app/controllers/experiences_controller.rb
@@ -0,0 +1,48 @@
+class ExperiencesController < ApplicationController
+  def index
+    @experience = Experience.all
+  end
+
+  def show
+    @experience = Experience.find(params[:id])
+  end
+
+  def new
+    @experience = Experience.new
+  end
+
+  def create
+    @experience = Experience.new(experiences_params)
+    if @education.save
+      redirect_to experience_path, success: "You have successfuly created a experience"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @experience = Experience.find(params[:id])
+  end
+
+  def update
+    @experience = Experience.find(params[:id])
+    respond_to do |format|
+      if @experience(experiences_params)
+        format.html { redirect_to @experience, notice: "The experience has been updated." }
+      else
+        format.html { render :edit }
+      end
+  end
+
+  def destroy
+    @experience = Experience.find(params[:id])
+    @experience.destroy
+    redirect_to experience_path, success: "You have successfuly deleted the experience entry."
+  end
+
+  private
+
+  def experiences_params 
+    params.require(:experiences).permit(:title, :description, :date)
+  end
+end
