7762d970690cb6049dc0076262edc63b14b279fd
Jose Chirivella <jrchg1403@gmail.com>
2015-10-25 19:45:04 -0400

    dropped table resumes and migrated.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,10 +1,4 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  context "GET home#index" do
-
-    setup { get :index }
-    should render_template('index')
-    should respond_with(:success)
-  end
 end
