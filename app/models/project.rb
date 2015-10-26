74a5a49d3d31035c87952854a3ec3d5871b770bb
Jose Chirivella <jrchg1403@gmail.com>
2015-10-25 23:35:52 -0400

    added test controller for home

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,6 @@ class Project < ActiveRecord::Base
 
   validates :title, :description, :live_url, :source_code_link, presence: true
 
+  validates_attachment :image
   has_attached_file :image, :styles => { :medium => "300x000>", :thumb => "100x100>" }
 end
