99bda52772e1d0b1e44fce3021f1411abf0b83f2
Jose Chirivella <jrchg1403@gmail.com>
2015-10-29 10:30:51 -0400

    removed first and last name from contacts. Added name

---
+++ db/migrate/20151029143021_add_name_to_contact.rb
@@ -0,0 +1,5 @@
+class AddNameToContact < ActiveRecord::Migration
+  def change
+    add_column :contacts, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029135753) do
+ActiveRecord::Schema.define(version: 20151029143021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "contacts", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
     t.string   "email"
     t.text     "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "educations", force: :cascade do |t|
