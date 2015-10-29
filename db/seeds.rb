72056629c7b0303206e6226b4ce71e1222dd4a6e
Jose Chirivella <jrchg1403@gmail.com>
2015-10-29 10:03:25 -0400

    Added better date format to education seeds lines

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Education.destroy_all
-Education.create!(title: 'The Iron Yard', description: 'Rails Engineering', date: '12-14-2014')
-Education.create!(title: 'Instituto Educacional Juan XXIII', description: 'High School Science Diploma', date: '06-24-2012')
+Education.create!(title: 'The Iron Yard', description: 'Rails Engineering', date: 'December 14, 2014')
+Education.create!(title: 'Instituto Educacional Juan XXIII', description: 'High School Science Diploma', date: 'Juny 06, 2012')
 
 Experience.destroy_all
 Experience.create!(title: 'Lead Software Developer', company: 'MATI Energy', description: 'MATI Project', date: 'February 19, 2015')
