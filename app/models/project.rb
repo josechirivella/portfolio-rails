b7d9262bdd170a4fb3bab2c40d5b7630a764c4a1
Jose Chirivella <jrchg1403@gmail.com>
2015-10-30 11:13:52 -0400

    removed paperclip image from projects. Added string for url

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,8 @@
 class Project < ActiveRecord::Base
 
-  validates :title, :description, :live_url, :source_code_link, presence: true
+  validates_presence_of :title, :description, :live_url, :source_code_link
 
   validates_attachment :image
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   has_attached_file :image, :styles => { :medium => "300x000>", :thumb => "100x100>" }
 end
