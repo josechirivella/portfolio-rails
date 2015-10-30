de2d4b0b275615ec93fc79697892b7e1e89ec2a3
Jose Chirivella <jrchg1403@gmail.com>
2015-10-30 16:35:37 -0400

    fixed admin fixtures and added presence of email and password for admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,4 +2,6 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :trackable, :validatable
+
+  validates_presence_of :email, :encrypted_password
 end
