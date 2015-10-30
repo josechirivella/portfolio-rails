1276863c6064e8944d76b50005bb876b6c956274
Jose Chirivella <jrchg1403@gmail.com>
2015-10-30 14:37:08 -0400

    Change the way I did the list on resumes and experience

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Education.create!(title: 'Instituto Educacional Juan XXIII', description: 'High 
 
 Experience.destroy_all
 Experience.create!(title: 'Lead Software Developer', company: 'MATI Energy', description: 'MATI Project', date: 'February 19, 2015')
-Experience.create!(title: 'Front End Developer - Intern', company: 'Windsor Circle', description: 'Front end developer', date: 'May 1, 2015')
+Experience.create!(title: 'Front End Developer - Intern', company: 'Windsor Circle', description: 'QA, Styling...', date: 'May 1, 2015')
 
 Project.destroy_all
 Project.create!(title: 'MATI Website', description: 'Redesigned the MATI Website', live_url: 'http://matienergy.com', source_code_link: 'http://www.matienergy.com', background_image_url: '')
