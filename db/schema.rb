247810abd1ea690bf245847cb3c4a795e1320e8d
Jose Chirivella <jrchg1403@gmail.com>
2015-11-02 22:07:07 -0500

    added end to finish the statement on schema, bug

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20151030184012) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-
+  end
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
