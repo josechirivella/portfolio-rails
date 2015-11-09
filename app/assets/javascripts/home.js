ef97bac6ee1d9102d5bdb95485ba4c2ba839acdc
Jose Chirivella <jrchg1403@gmail.com>
2015-11-08 22:33:55 -0500

    added js code and css, works for now, needs styling

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,31 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+// Modal
+$(document).ready(function() {
+
+    //select all the a tag with name equal to modal
+    $('a[name=modal]').click(function(e) {
+        //Cancel the link behavior
+        e.preventDefault();
+        //Get the A tag
+        var id = $(this).attr('href');
+
+        //Get the window height and width
+        var winH = $(window).height();
+        var winW = $(window).width();
+
+        //Set the popup window to center
+        $(id).css('top',  winH/2-$(id).height()/2);
+        $(id).css('left', winW/2-$(id).width()/2);
+
+        //transition effect
+        $(id).fadeIn(500);
+
+    });
+
+    //if close button is clicked
+    $('.modalwindow .close').click(function (e) {
+        //Cancel the link behavior
+        e.preventDefault();
+        $('.modalwindow').fadeOut(500);
+    });
+
+});
