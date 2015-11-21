00327ba2b851ff1d9b06394f8e9d73a389095833
Jose Chirivella <jrchg1403@gmail.com>
2015-11-20 23:27:25 -0500

    mah

--- assets_precompile.sh
+++ assets_precompile.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-echo This script its going to clean the assets folder and precompile the new assets. Be patient.
+echo This script its going to clean the assets folder and precompile the new assets.
 rake assets:clean RAILS_ENV="production"
 rake assets:precompile RAILS_ENV="production"
 
