316d93b0005f6aadd614104b09d00e0d9eb24a21
Jose Chirivella <jrchg1403@gmail.com>
2016-05-04 23:45:31 -0400

    removed white spacing and single quotes

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -1,5 +1,4 @@
 require 'test_helper'
 
 class ContactsControllerTest < ActionController::TestCase
-
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  context "GET home#index" do
+  context 'GET home#index' do
     setup { get :index }
     should render_template('index')
     should respond_with(200)
