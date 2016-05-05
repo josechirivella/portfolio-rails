421440e561844ffa57837a6bc1a809f93875fd53
Jose Chirivella <jrchg1403@gmail.com>
2016-05-04 23:49:35 -0400

    Added missing testing for admin and missing validation for background image on project

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should validate_presence_of(:email)
+  should validate_presence_of(:encrypted_password)
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -5,4 +5,5 @@ class ProjectTest < ActiveSupport::TestCase
   should validate_presence_of(:description)
   should validate_presence_of(:advance_description)
   should validate_presence_of(:live_url)
+  should validate_presence_of(:background_image_url)
 end
