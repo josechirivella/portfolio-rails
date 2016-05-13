2efe01642504eab8284a7a2e05586801ab7da3df
Jose <jrchg1403@gmail.com>
2016-05-12 21:48:08 -0400

    Added id to fixtures

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -1,4 +1,9 @@
 require 'test_helper'
 
 class ProjectsControllerTest < ActionController::TestCase
+  context 'GET projects#show' do
+    setup { get :show, :id => 1 }
+    should render_template('show')
+    should respond_with(200)
+  end
 end
