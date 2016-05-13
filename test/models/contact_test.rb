4ac69a6057c8bb7421132b70736b94e96978f0c6
Jose <jrchg1403@gmail.com>
2016-05-12 21:39:56 -0400

    added testing for contacts, should allow value for email

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -4,4 +4,6 @@ class ContactTest < ActiveSupport::TestCase
   should validate_presence_of(:name)
   should validate_presence_of(:email)
   should validate_presence_of(:message)
+
+  should allow_value('hello@domain.com').for(:email)
 end
