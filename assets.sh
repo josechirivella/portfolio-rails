8db45d95c8134df47518bd9392f86888163a076a
Jose <jrchg1403@gmail.com>
2016-08-03 21:14:01 -0400

    Removed sudo

--- assets.sh
+++ assets.sh
@@ -2,5 +2,5 @@
 echo This script its going to clean the assets folder and precompile the new assets.
 rake assets:clean RAILS_ENV='production'
 rake assets:precompile RAILS_ENV='production'
-sudo service nginx restart
+service nginx restart
 
