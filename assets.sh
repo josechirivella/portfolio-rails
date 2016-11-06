4e5aaff347815911be3b4a94b27b304df2e7abf1
Jose Chirivella <jrchg1403@gmail.com>
2016-11-06 01:34:32 -0400

    updated script

--- assets.sh
+++ assets.sh
@@ -2,4 +2,3 @@
 echo This script its going to clean the assets folder and precompile the new assets.
 rake assets:clean RAILS_ENV='production'
 rake assets:precompile RAILS_ENV='production'
-service nginx restart
