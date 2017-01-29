1002dff1762dd4ff3b750b00db13db6a8839d5fc
Jose Chirivella <jrchg1403@gmail.com>
2017-01-29 01:13:33 -0500

    migration and test

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,8 +1,3 @@
 class Experience < ActiveRecord::Base
 	validates_presence_of :title, :company, :description, :from_date
-	if self.present === false
-		validate_presence_of :until
-	else
-		validate_presence_of :present
-	end
 end
