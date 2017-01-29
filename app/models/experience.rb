56812266eb51a9dd7dfab3a4d63e22877efcd0ea
Jose Chirivella <jrchg1403@gmail.com>
2017-01-29 00:03:51 -0500

    validation

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,3 +1,8 @@
 class Experience < ActiveRecord::Base
-  validates_presence_of :title, :company, :description, :from_date, :until
+	validates_presence_of :title, :company, :description, :from_date
+	if self.present === false
+		validate_presence_of :until
+	else
+		validate_presence_of :present
+	end
 end
